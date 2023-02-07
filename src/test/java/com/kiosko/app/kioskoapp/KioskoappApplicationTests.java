7aa7ca801b8b0c5cc803b1dbddbc7a3abb47c7b1
Juan Pablo Correa Tarazona <89850618+JuanPCT@users.noreply.github.com>
2023-02-07 13:15:39 -0500

    Estructura inicial

---
+++ src/test/java/com/kiosko/app/kioskoapp/KioskoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiosko.app.kioskoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
