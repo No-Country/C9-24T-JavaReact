ea422e2fcea7e94ae69b7498469b32bc50ee515b
Dedwison <dedwison@gmail.com>
2023-02-10 16:50:44 -0300

    subo cambios

---
+++ client/src/component/pages/ProductView/index.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import CardProduct from "../../Common/CardProduct";
+/* import NavBar from "../../Layout/NavBar"; */
+
+export default function ProductView() {
+  return (
+    <>
+      <CardProduct />
+      {/* <NavBar /> */}
+    </>
+  );
+}
--- client/src/router/RouteController.jsx
+++ client/src/router/RouteController.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../component/Home";
-import Description from "../component/DescriptionProduct";
+import Description from "../component/pages/DescriptionVew";
+
+import ProductView from "../component/pages/ProductView";
 
 function RouteController() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/description" element={<Description />} />
+      <Route path="/product" element={<ProductView />} />
     </Routes>
   );
 }
