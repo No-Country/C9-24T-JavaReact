d3b9da090fbd3d4e7c2abe625d1a4a045a8996cc
Lihuel <lihuelmujica@gmail.com>
2023-02-12 03:56:16 -0300

    Agregar findByNombre a repository

--- src/main/java/com/kiosko/app/kioskoapp/repository/CaracteristicaRepository.java
+++ src/main/java/com/kiosko/app/kioskoapp/repository/CaracteristicaRepository.java
@@ -4,6 +4,9 @@ import com.kiosko.app.kioskoapp.entities.Caracteristica;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CaracteristicaRepository extends JpaRepository<Caracteristica, Integer> {
+    Optional<Caracteristica> findByNombre(String nombre);
 }
