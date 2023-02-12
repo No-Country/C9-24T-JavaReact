37fddd79798ec42d82a2c6e97e95ef988d2bcb04
Lihuel <lihuelmujica@gmail.com>
2023-02-12 03:30:05 -0300

    Agregar findByNombre a Categoria Repository

--- src/main/java/com/kiosko/app/kioskoapp/repository/CategoriaRepository.java
+++ src/main/java/com/kiosko/app/kioskoapp/repository/CategoriaRepository.java
@@ -3,5 +3,8 @@ package com.kiosko.app.kioskoapp.repository;
 import com.kiosko.app.kioskoapp.entities.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+    Optional<Categoria> findByNombre(String nombre);
 }
