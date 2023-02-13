5d382b09a4cfd654c45ebad85c7b2c775bfc41cc
Dedwison <dedwison@gmail.com>
2023-02-13 13:30:36 -0300

    feat: navBar

--- client/src/component/Layout/NavBar/index.jsx
+++ client/src/component/Layout/NavBar/index.jsx
@@ -2,25 +2,28 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import RestoreIcon from "@mui/icons-material/Restore";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
+// import RestoreIcon from "@mui/icons-material/Restore";
+// import FavoriteIcon from "@mui/icons-material/Favorite";
+// import LocationOnIcon from "@mui/icons-material/LocationOn";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
 
 export default function NavBar() {
-  const [value, setValue] = React.useState(0);
+  //   const [value, setValue] = React.useState(0);
 
   return (
-    <Box sx={{ width: 500 }}>
+    <Box sx={{ width: 360 }}>
       <BottomNavigation
         showLabels
-        value={value}
-        onChange={(event, newValue) => {
+        // value={value}
+        /* onChange={(event, newValue) => {
           setValue(newValue);
-        }}
+        }} */
       >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+        <BottomNavigationAction label="Buscar" icon={<SearchIcon />} />
+        <BottomNavigationAction label="Cuenta" icon={<PersonIcon />} />
       </BottomNavigation>
     </Box>
   );
