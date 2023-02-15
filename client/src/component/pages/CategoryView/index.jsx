5b52e5ff23d87f7f9e080a0d6da1b3e32710bd3f
luisvelark <velark07@gmail.com>
2023-02-15 18:43:56 -0300

    posicionar navbar abajo

--- client/src/component/pages/CategoryView/index.jsx
+++ client/src/component/pages/CategoryView/index.jsx
@@ -8,128 +8,199 @@ import { blue } from "@mui/material/colors";
 
 export default function CategoryView() {
   return (
-    <Stack sx={{ margin: 0, pading: 0 }}>
+    <Stack sx={{ margin: 0, pading: 0, height: "100vh" }}>
       <MySearchBar />
-      <Typography
-        variant="h6"
-        sx={{ textAlign: "left", padding: "0.5em 0 0 0.5em" }}
-      >
-        Categorias
-      </Typography>
-      <Container sx={{ margin: 0, padding: "0.5em" }}>
-        <Grid
-          container
-          spacing={0.5}
-          sx={{ width: 340, margin: 0, padding: 0 }}
+      <Box>
+        <Typography
+          variant="h6"
+          sx={{ textAlign: "left", padding: "0.5em 0 0 0.5em" }}
         >
-          {/* <CategoryCard />
+          Categorias
+        </Typography>
+        <Container sx={{ margin: 0, padding: "0.5em" }}>
+          <Grid
+            container
+            spacing={0.5}
+            sx={{ width: 340, margin: 0, padding: 0 }}
+          >
+            {/* <CategoryCard />
           <CategoryCard />
           <CategoryCard />
           <CategoryCard /> */}
-          <Grid
-            sx={{
-              borderRadius: "15px",
-              // background: "blue",
-              margin: 0,
-              marginBottom: "0.5em",
-              padding: 0,
-              boxShadow: "none",
-              width: "160px",
-              height: "120px",
-            }}
-            item
-            xs={6}
-            md={8}
-          >
-            <CategoryCard />
+            <Grid
+              sx={{
+                borderRadius: "15px",
+                // background: "blue",
+                margin: 0,
+                marginBottom: "0.5em",
+                padding: 0,
+                boxShadow: "none",
+                width: "160px",
+                height: "120px",
+              }}
+              item
+              xs={6}
+              md={8}
+            >
+              <CategoryCard />
+            </Grid>
+            <Grid
+              sx={{
+                borderRadius: "15px",
+                // background: "blue",
+                margin: 0,
+                marginBottom: "0.5em",
+                padding: 0,
+                boxShadow: "none",
+                width: "160px",
+                height: "120px",
+              }}
+              item
+              xs={6}
+              md={8}
+            >
+              <CategoryCard />
+            </Grid>
+            <Grid
+              sx={{
+                borderRadius: "15px",
+                // background: "blue",
+                margin: 0,
+                marginBottom: "0.5em",
+                pading: 0,
+                boxShadow: "none",
+                width: "160px",
+                height: "120px",
+              }}
+              item
+              xs={6}
+              md={8}
+            >
+              <CategoryCard />
+            </Grid>
+            <Grid
+              sx={{
+                borderRadius: "15px",
+                // background: "blue",
+                margin: 0,
+                marginBottom: "0.5em",
+                pading: 0,
+                boxShadow: "none",
+                width: "160px",
+                height: "120px",
+              }}
+              item
+              xs={6}
+              md={8}
+            >
+              <CategoryCard />
+            </Grid>
+            <Grid
+              sx={{
+                borderRadius: "15px",
+                // background: "blue",
+                margin: 0,
+                marginBottom: "0.5em",
+                pading: 0,
+                boxShadow: "none",
+                width: "160px",
+                height: "120px",
+              }}
+              item
+              xs={6}
+              md={8}
+            >
+              <CategoryCard />
+            </Grid>
+            <Grid
+              sx={{
+                borderRadius: "15px",
+                // background: "blue",
+                margin: 0,
+                marginBottom: "0.5em",
+                pading: 0,
+                boxShadow: "none",
+                width: "160px",
+                height: "120px",
+              }}
+              item
+              xs={6}
+              md={8}
+            >
+              <CategoryCard />
+            </Grid>
+            <Grid
+              sx={{
+                borderRadius: "15px",
+                // background: "blue",
+                margin: 0,
+                marginBottom: "0.5em",
+                pading: 0,
+                boxShadow: "none",
+                width: "160px",
+                height: "120px",
+              }}
+              item
+              xs={6}
+              md={8}
+            >
+              <CategoryCard />
+            </Grid>
+            <Grid
+              sx={{
+                borderRadius: "15px",
+                // background: "blue",
+                margin: 0,
+                marginBottom: "0.5em",
+                pading: 0,
+                boxShadow: "none",
+                width: "160px",
+                height: "120px",
+              }}
+              item
+              xs={6}
+              md={8}
+            >
+              <CategoryCard />
+            </Grid>
+            <Grid
+              sx={{
+                borderRadius: "15px",
+                // background: "blue",
+                margin: 0,
+                marginBottom: "0.5em",
+                pading: 0,
+                boxShadow: "none",
+                width: "160px",
+                height: "120px",
+              }}
+              item
+              xs={6}
+              md={8}
+            >
+              <CategoryCard />
+            </Grid>
+            <Grid
+              sx={{
+                borderRadius: "15px",
+                // background: "blue",
+                margin: 0,
+                marginBottom: "0.5em",
+                pading: 0,
+                boxShadow: "none",
+                width: "160px",
+                height: "120px",
+              }}
+              item
+              xs={6}
+              md={8}
+            >
+              <CategoryCard />
+            </Grid>
           </Grid>
-          <Grid
-            sx={{
-              borderRadius: "15px",
-              // background: "blue",
-              margin: 0,
-              marginBottom: "0.5em",
-              padding: 0,
-              boxShadow: "none",
-              width: "160px",
-              height: "120px",
-            }}
-            item
-            xs={6}
-            md={8}
-          >
-            <CategoryCard />
-          </Grid>
-          <Grid
-            sx={{
-              borderRadius: "15px",
-              // background: "blue",
-              margin: 0,
-              marginBottom: "0.5em",
-              pading: 0,
-              boxShadow: "none",
-              width: "160px",
-              height: "120px",
-            }}
-            item
-            xs={6}
-            md={8}
-          >
-            <CategoryCard />
-          </Grid>
-          <Grid
-            sx={{
-              borderRadius: "15px",
-              // background: "blue",
-              margin: 0,
-              marginBottom: "0.5em",
-              pading: 0,
-              boxShadow: "none",
-              width: "160px",
-              height: "120px",
-            }}
-            item
-            xs={6}
-            md={8}
-          >
-            <CategoryCard />
-          </Grid>
-          <Grid
-            sx={{
-              borderRadius: "15px",
-              // background: "blue",
-              margin: 0,
-              marginBottom: "0.5em",
-              pading: 0,
-              boxShadow: "none",
-              width: "160px",
-              height: "120px",
-            }}
-            item
-            xs={6}
-            md={8}
-          >
-            <CategoryCard />
-          </Grid>
-          <Grid
-            sx={{
-              borderRadius: "15px",
-              // background: "blue",
-              margin: 0,
-              marginBottom: "0.5em",
-              pading: 0,
-              boxShadow: "none",
-              width: "160px",
-              height: "120px",
-            }}
-            item
-            xs={6}
-            md={8}
-          >
-            <CategoryCard />
-          </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </Box>
+
       <NavBar sx={{ alignSelf: "flex-end" }} />
     </Stack>
   );
