ca7bffcfe7832b21d7348e2f22f18d27804e9c1f
Lihuel <lihuelmujica@gmail.com>
2023-02-16 12:32:20 -0300

    Agregar método findByIdProducto

--- src/main/java/com/kiosko/app/kioskoapp/repository/ImagenRepository.java
+++ src/main/java/com/kiosko/app/kioskoapp/repository/ImagenRepository.java
@@ -3,6 +3,8 @@ package com.kiosko.app.kioskoapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.kiosko.app.kioskoapp.entities.ImagenesProductos;
 
+import java.util.List;
+
 public interface ImagenRepository extends JpaRepository<ImagenesProductos, Integer>{
-    
+    List<ImagenesProductos> findByIdProducto (int idProducto);
 }
