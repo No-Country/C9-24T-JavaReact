17d5e5c1f33e8d758aad01de3b6197a198fb28c8
Dedwison <dedwison@gmail.com>
2023-02-18 18:50:31 -0300

    act datos

--- client/src/redux/action/index.js
+++ client/src/redux/action/index.js
@@ -1,3 +1,18 @@
 import axios from "axios";
+import BDproducts from "../../assets/BD.json";
 
-const urlAPI = "http://localhost:3001";
+const urlAPI = "BDproducts";
+
+export function getProduct() {
+  return async function (dispatch) {
+    try {
+      const product = await axios.get(`${urlAPI}/product`);
+      dispatch({
+        type: "GET_PRODUCT",
+        payload: product.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
