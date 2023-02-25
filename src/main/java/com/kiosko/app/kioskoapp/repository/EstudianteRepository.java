120673ee46cbb2df3cf77588b20dcf172c730271
Lihuel <lihuelmujica@gmail.com>
2023-02-25 00:08:45 -0300

    Crear estudiante repository

---
+++ src/main/java/com/kiosko/app/kioskoapp/repository/EstudianteRepository.java
@@ -0,0 +1,12 @@
+package com.kiosko.app.kioskoapp.repository;
+
+import com.kiosko.app.kioskoapp.entities.Estudiante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EstudianteRepository extends JpaRepository<Estudiante, Integer> {
+    Optional<Estudiante> findByUsuarioId(String id_usuario);
+}
