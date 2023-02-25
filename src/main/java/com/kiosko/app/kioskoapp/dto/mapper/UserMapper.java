29b1882e98f59c671be0e9668b14afff4ccb29f4
Lihuel <lihuelmujica@gmail.com>
2023-02-25 00:58:31 -0300

    Agregar método userProfileToAppUser en UserMapper

--- src/main/java/com/kiosko/app/kioskoapp/dto/mapper/UserMapper.java
+++ src/main/java/com/kiosko/app/kioskoapp/dto/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.kiosko.app.kioskoapp.dto.mapper;
 import com.kiosko.app.kioskoapp.dto.UserProfile;
 import com.kiosko.app.kioskoapp.dto.UserRegistration;
 import com.kiosko.app.kioskoapp.entities.AppUser;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -14,6 +15,8 @@ public interface UserMapper {
     @Mapping(source = "appUserRole", target = "rol")
     UserProfile appUserToUserProfile (AppUser appUser);
     AppUser userRegistrationToAppUser(UserRegistration userRegistration);
+    @InheritInverseConfiguration
+    AppUser userProfileToAppUser(UserProfile userProfile);
 
 
 
