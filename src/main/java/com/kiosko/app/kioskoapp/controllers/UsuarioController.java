d2a748a3697bb11ad010c9cce823e09df7fcb657
Lihuel <lihuelmujica@gmail.com>
2023-02-25 00:59:12 -0300

    Crear Estudiante Controller

--- src/main/java/com/kiosko/app/kioskoapp/controllers/UsuarioController.java
+++ src/main/java/com/kiosko/app/kioskoapp/controllers/UsuarioController.java
@@ -1,12 +1,16 @@
 package com.kiosko.app.kioskoapp.controllers;
 
+import com.kiosko.app.kioskoapp.dto.EstudianteDTO;
 import com.kiosko.app.kioskoapp.dto.Rol;
 import com.kiosko.app.kioskoapp.dto.UserProfile;
 import com.kiosko.app.kioskoapp.dto.UserRegistration;
 import com.kiosko.app.kioskoapp.entities.AppUser;
+import com.kiosko.app.kioskoapp.exception.BadRequestException;
+import com.kiosko.app.kioskoapp.exception.ForbidenException;
 import com.kiosko.app.kioskoapp.exception.ResourceAlreadyExistsException;
 import com.kiosko.app.kioskoapp.exception.ResourceNotFoundException;
 import com.kiosko.app.kioskoapp.service.IAppUserService;
+import com.kiosko.app.kioskoapp.service.IEstudianteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,16 +20,19 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/usuario")
 @CrossOrigin(origins="*", allowedHeaders = "*")
 public class UsuarioController {
     private final IAppUserService userService;
+    private final IEstudianteService estudianteService;
 
     @Autowired
-    public UsuarioController(IAppUserService userService) {
+    public UsuarioController(IAppUserService userService, IEstudianteService estudianteService) {
         this.userService = userService;
+        this.estudianteService = estudianteService;
     }
 
     @GetMapping
@@ -39,15 +46,33 @@ public class UsuarioController {
         return new ResponseEntity<>(userService.create(userRegistration, Rol.PADRE), HttpStatus.CREATED);
     }
 
-    @GetMapping("/padre{dni}")
-    public ResponseEntity<List<UserProfile>> getAllHijos(@PathVariable("dni") String dni) {
-        return new ResponseEntity<>(HttpStatus.OK);
+    @GetMapping("/padre/{dni}")
+    public ResponseEntity<UserProfile> getPadreByDni(@PathVariable("dni") String dni) throws ResourceNotFoundException, ForbidenException {
+        AppUser user = getUser();
+        if(!Objects.equals(user.getDni(), dni) || !Objects.equals(user.getAppUserRole().getNombre(), Rol.ADMIN.toString())) throw new ForbidenException("");
+        return new ResponseEntity<>(userService.getProfileByDni(dni), HttpStatus.OK);
     }
 
-    @PostMapping("/padre/{dni}")
-    public ResponseEntity<UserProfile> registerHijo(@RequestBody UserRegistration userRegistration, @PathVariable("dni") String dni) {
-        return new ResponseEntity<>(HttpStatus.OK);
+    @GetMapping("/padre/{dni}/estudiante")
+    public ResponseEntity<List<EstudianteDTO>> getAllEstudiantesByPadre(@PathVariable("dni") String dni) throws ForbidenException {
+        AppUser user = getUser();
+        if(!Objects.equals(user.getDni(), dni) || !Objects.equals(user.getAppUserRole().getNombre(), Rol.ADMIN.toString())) throw new ForbidenException("");
+        return new ResponseEntity<>(estudianteService.getAllEstudiantesByDniPadre(dni), HttpStatus.OK);
     }
+
+    @PostMapping("/padre/{dni}/estudiante")
+    public ResponseEntity<EstudianteDTO> registerEstudiante(@PathVariable("dni") String dniPadre, @RequestBody UserRegistration userRegistration) throws ResourceAlreadyExistsException, ResourceNotFoundException, BadRequestException {
+        return new ResponseEntity<>(estudianteService.createEstudiante(userRegistration, dniPadre), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/estudiante/{dni}")
+    public ResponseEntity<EstudianteDTO> getEstudianteByDni(@PathVariable("dni") String dni) throws ResourceNotFoundException, ForbidenException {
+        AppUser user = getUser();
+        EstudianteDTO estudianteDTO = estudianteService.getEstudianteByDni(dni);
+        if(!Objects.equals(user.getDni(), dni) || !Objects.equals(user.getAppUserRole().getNombre(), Rol.ADMIN.toString())) throw new ForbidenException("");
+        return new ResponseEntity<>(estudianteService.getEstudianteByDni(dni), HttpStatus.OK);
+    }
+
 
     private AppUser getUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -55,7 +80,6 @@ public class UsuarioController {
     }
 
     private String getUserEmail() {
-        System.out.println(SecurityContextHolder.getContext().getAuthentication().getName());
         return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 }
