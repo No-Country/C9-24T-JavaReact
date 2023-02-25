138e31916f57800023f70f77565a40efe87ae31c
Lihuel <lihuelmujica@gmail.com>
2023-02-25 00:57:31 -0300

    Modificar métodos de EstudianteRepository para buscar por dni

--- src/main/java/com/kiosko/app/kioskoapp/repository/EstudianteRepository.java
+++ src/main/java/com/kiosko/app/kioskoapp/repository/EstudianteRepository.java
@@ -4,10 +4,11 @@ import com.kiosko.app.kioskoapp.entities.Estudiante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EstudianteRepository extends JpaRepository<Estudiante, Integer> {
-    Optional<Estudiante> findByUsuarioId(String id_usuario);
-    Optional<Estudiante> findByPadreId(String id_padre);
+    Optional<Estudiante> findByUsuarioDni(String dni);
+    List<Estudiante> findByPadreDni(String dniPadre);
 }
