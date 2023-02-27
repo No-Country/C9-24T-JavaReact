c3a292dd88b113e8d22a44d96085c22570d746ed
Lihuel <lihuelmujica@gmail.com>
2023-02-27 13:59:54 -0300

    Crear y Mapear Carga con base de datos

---
+++ src/main/java/com/kiosko/app/kioskoapp/entities/Carga.java
@@ -0,0 +1,28 @@
+package com.kiosko.app.kioskoapp.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "cargas_saldo")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Carga {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    @ManyToOne
+    @JoinColumn(name = "id_estudiante")
+    private Estudiante estudiante;
+    @ManyToOne
+    @JoinColumn(name = "id_padre")
+    private AppUser padre;
+    @Column(name = "cantidad")
+    private BigDecimal monto;
+    private String estado;
+}
